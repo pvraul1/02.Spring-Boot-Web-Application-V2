bff795daecb668f08eda5ebc919fae0cd0317806
Raul Perez <raul.perez.vicente@gmail.com>
2022-07-24 10:04:45 +0200

    33. Step 08 - Capturing QueryParams using RequestParam

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,7 +1,11 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * The login controller class.
@@ -11,11 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @version Jul 24, 2022 - 9:40:47 AM
  *
  */
+@Slf4j
 @Controller
 public class LoginController {
 
     @RequestMapping("login")
-    public String goToLoginPage() {
+    public String goToLoginPage(@RequestParam final String name, ModelMap model) {
+        model.put("name", name);
+        log.info(String.format("Request param is %s", name));
+
         return "login";
     }
 
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -4,6 +4,6 @@
 <title>Login Page</title>
 </head>
 <body>
-	Welcome to the login page!
+	Welcome to the login page ${name}!
 </body>
 </html>
