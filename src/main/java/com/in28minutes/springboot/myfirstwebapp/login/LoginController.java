6b6083d509e9314312c117b94c156ecacb0ca73c
Raul Perez <raul.perez.vicente@gmail.com>
2022-07-24 13:09:34 +0200

    37. Step 11 - Creating a Login Form

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,11 +1,7 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * The login controller class.
@@ -18,16 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
-
     @RequestMapping("login")
-    public String goToLoginPage(@RequestParam final String name, ModelMap model) {
-        model.put("name", name);
-
-        if (logger.isDebugEnabled()) {
-            logger.debug(String.format("Request param is %s", name));
-        }
-
+    public String goToLoginPage() {
         return "login";
     }
 
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -4,6 +4,11 @@
 <title>Login Page</title>
 </head>
 <body>
-	Welcome to the login page ${name}!
+	Welcome to the login page!
+	<form method="post">
+		Name: <input type="text" name="name"><br>
+		Password: <input type="password" name="password"><br>
+		<input type="submit"><br>
+	</form>
 </body>
 </html>
