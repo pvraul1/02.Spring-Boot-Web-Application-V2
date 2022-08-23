3d326e97ec293ccd27ba8c80bf7ee7f6f7c038c3
Raul Perez <raul.perez.vicente@gmail.com>
2022-08-23 20:01:32 +0200

    49. Step 23 - Using Command Beans to implement New Todo Page Validations

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -19,7 +20,10 @@ public class Todo {
 
     private int id;
     private String username;
+
+    @Size(min=10, message="Enter atleast 10 characters")
     private String description;
+
     private LocalDate targetDate;
     private boolean done;
 
