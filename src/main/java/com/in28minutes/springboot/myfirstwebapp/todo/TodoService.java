543f5890927fc80f0efb68373f7bbd7d0f053ad8
Raul Perez <raul.perez.vicente@gmail.com>
2022-08-24 17:21:00 +0200

    50. Step 24 - Implementing Delete Todo Feature - New View

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
@@ -33,6 +34,11 @@ public class TodoService {
 
     public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
         todos.add(new Todo(++todosCount, username, description, targetDate, done));
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
     }
 
 }
