180bd190d50806067fd133671b40c0c2157f5fb2
Raul Perez <raul.perez.vicente@gmail.com>
2022-09-01 17:34:32 +0200

    66. Step 36 - Making Todo an Entity and Population Todo Data into H2

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -2,6 +2,9 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,9 +19,13 @@ import lombok.Data;
  */
 @Data
 @AllArgsConstructor
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue
     private int id;
+
     private String username;
 
     @Size(min=10, message="Enter atleast 10 characters")
